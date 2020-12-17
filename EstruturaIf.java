7cbef5ca83e7d93ba0fac90d1073ab69da8e9862
EdsonSantosLimaJava <76164900+EdsonSantosLimaJava@users.noreply.github.com>
2020-12-17 13:44:33 -0300

    Add files via upload

---
+++ EstruturaIf.java
@@ -0,0 +1,14 @@
+
+public class EstruturaIf {
+	
+	public static void main(String[] args) {
+		
+//		System.out.println("Estrutura de decisao if");
+		
+		for (int i = 0; i <= 10; i++) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
